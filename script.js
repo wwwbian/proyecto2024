7dfea7bcb3fe941f722a3693a5231036ed3c9659
camigonzalez568 <cg3857722@gmail.com>
2024-03-04 10:15:28 -0300

    hola

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Seleccionamos todas las imágenes con la clase "elementos" y les asignamos el evento de clic
 const imagenes = document.querySelectorAll('.elementos img');
 
 imagenes.forEach(function(imagen) {
     imagen.addEventListener('click', function() {
-        // Toggle: si la imagen tiene la clase "blur", la quitamos, si no la tiene, la añadimos
         this.classList.toggle('blur');
     });
 });
