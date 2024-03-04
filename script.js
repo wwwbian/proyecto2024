c88c37d9b87935fc114d90e88095a0b2eafa2310
camigonzalez568 <cg3857722@gmail.com>
2024-03-04 11:04:18 -0300

    seleccion de img

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 const imagenes = document.querySelectorAll('.elementos img');
+const imagenes2 = document.querySelectorAll('.elementos2 img');
 
 imagenes.forEach(function(imagen) {
     imagen.addEventListener('click', function() {
-        this.classList.toggle('blur');
+        // Desenfocar todos los elementos
+        imagenes.forEach(function(imagen) {
+            imagen.classList.add('blur');
+        });
+        // Quitar el desenfoque del elemento clicado
+        this.classList.remove('blur');
     });
 });
+
+imagenes2.forEach(function(imagen2) {
+    imagen2.addEventListener('click', function() {
+        // Desenfocar todos los elementos
+        imagenes2.forEach(function(imagen2) {
+            imagen2.classList.add('blur');
+        });
+        // Quitar el desenfoque del elemento clicado
+        this.classList.remove('blur');
+    });
+});
+
+function manejarClic(elementos, elementoClicado) {
+    elementos.forEach(function(elemento) {
+        elemento.classList.add('blur');
+    });
+    elementoClicado.classList.remove('blur');
+}
+
 
